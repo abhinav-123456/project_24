e3130b2cba13173564ca4815053699a4d0d5b5c6
abhinav-123456 <70750515+abhinav-123456@users.noreply.github.com>
2020-11-25 19:15:02 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,21 @@
+class PAPER{
+constructor(x,y,width){
+    var options = {
+        isStatic:false,
+        restitution:0.3,
+        friction:0.5,
+        density:1.2
+    }
+this.body = Bodies.circle(x,y,width,options)
+this.width = width;
+World.add(world,this.body);
+
+}
+display(){
+    var pos =this.body.position;
+    ellipseMode(CENTER);
+    fill(255);
+    ellipse(pos.x,pos.y,this.width);
+}
+
+};
